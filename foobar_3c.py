45ab0ceca9c1f589955a25892f539568e61e1fdb
Keegan <McWillkd@gmail.com>
2021-09-29 19:02:37 +0900

    Add files via upload

--- foobar_3c.py
+++ foobar_3c.py
@@ -2,6 +2,7 @@ from fractions import Fraction
 from fractions import gcd
 from functools import reduce
 
+
 def solution(m):
     # I heavily relied on the following website for guidence in writing the Markov Chain solution
     # https://brilliant.org/wiki/absorbing-markov-chains/
@@ -222,16 +223,65 @@ def solution(m):
             raise ArithmeticError("Matrix inverse out of tolerance.")
 
     # https://stackoverflow.com/questions/37237954/calculate-the-lcm-of-a-list-of-given-numbers-in-python/54597248
-
     def find_lcd(lst):
         lcm = 1
         for i in lst:
             lcm = lcm * i // gcd(lcm, i)
         return lcm
 
+    # https://github.com/mkutny/absorbing-markov-chains/blob/master/amc.py
+    # swap i,j rows/columns of a square matrix `m`
+    def swap(m, i, j):
+        n = []
+        s = len(m)
+
+        if s != len(m[0]):
+            raise Exception("Cannot swap non-square matrix")
+
+        if i == j:
+            # no need to swap
+            return m
+
+        for r in range(s):
+            nRow = []
+            tmpRow = m[r]
+            if r == i:
+                tmpRow = m[j]
+            if r == j:
+                tmpRow = m[i]
+            for c in range(s):
+                tmpEl = tmpRow[c]
+                if c == i:
+                    tmpEl = tmpRow[j]
+                if c == j:
+                    tmpEl = tmpRow[i]
+                nRow.append(tmpEl)
+            n.append(nRow)
+        return n
+    # reorganize matrix so zero-rows go last (preserving zero rows order)
+    def sort(m):
+        size = len(m)
+
+        zero_row = -1
+        for r in range(size):
+            sum = 0
+            for c in range(size):
+                sum += m[r][c]
+            if sum == 0:
+                # we have found all-zero row, remember it
+                zero_row = r
+            if sum != 0 and zero_row > -1:
+                # we have found non-zero row after all-zero row - swap these rows
+                n = swap(m, r, zero_row)
+                # and repeat from the begining
+                return sort(n)
+        # nothing to sort, return
+        return m
+
     # below is orginal code used to solve an absorbing markov chain
 
     def coerce_matrix(matrix):
+        matrix = sort(matrix)
         prob_matrix = []
         i = 0
         j = 0
@@ -270,14 +320,15 @@ def solution(m):
             for j in range(R_cols):
                 R[i][j] = prob_matrix[i][-j - 1]
         return prob_matrix, Q, R
-    # start of the active code  
-    
+
+    # start of the active code
+
     # catch case for when only the first row is filled and all other rows are zero
     zero_count = 0
     for row in m[1:]:
         if all(not x for x in row):
             zero_count += 1
-                  
+
     if zero_count == len(m[1:]):
         prob_array = []
         num_list = []
@@ -287,29 +338,29 @@ def solution(m):
         # make the values into probabilities
         for ele in m[0][1:]:
             prob_array.append(float(ele) / float(sum(m[0])))
-        # make the probabilites into a fraction     
+        # make the probabilites into a fraction
         for ele in prob_array:
             frac = Fraction(ele).limit_denominator(1000)
             num_list.append(int(frac.numerator))
             den_list.append(int(frac.denominator))
         lcd = find_lcd(den_list)
-        
+
         for ele in den_list:
             scale_list.append(lcd / ele)
-        
+
         for i in range(len(num_list)):
             scaled_num.append(int(num_list[i] * scale_list[i]))
-            
+
         scaled_num.reverse()
         output = scaled_num + [lcd]
-        
+
         return output
 
     # this is an important step where we coerce the matrix into its prob matrix and sub components for solving the markov chain
     m_prob, Q, R = coerce_matrix(m)
-    
+
     # catch case for when the determinant is zero
-    
+
     if determinant(matrix_subtraction(identity_matrix(len(Q)), Q)) == 0:
         prob_array = []
         all_zero_check = not all(not x for x in m_prob[0])
@@ -319,25 +370,25 @@ def solution(m):
         print(str(prob_array) + str('hello!'))
         output = []
         return output
-        
+
     if zero_count != len(m[1:]):
         N = invert_matrix(matrix_subtraction(identity_matrix(len(Q)), Q), tol=1)
         M = matrix_multiply(N, R)
-        
+
         num_list = []
         den_list = []
         scale_list = []
         scaled_num = []
-        
+
         for ele in M[0]:
             frac = Fraction(ele).limit_denominator(1000)
             num_list.append(int(frac.numerator))
             den_list.append(int(frac.denominator))
         lcd = find_lcd(den_list)
-        
+
         for ele in den_list:
             scale_list.append(lcd / ele)
-        
+
         for i in range(len(num_list)):
             scaled_num.append(int(num_list[i] * scale_list[i]))
         scaled_num.reverse()
@@ -345,7 +396,9 @@ def solution(m):
         return output
 
 
-print(solution([[0,1,0,0,0,1],[4,0,0,3,2,0],[0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0]]))
-print(solution([[0, 2, 1, 0, 0], [0, 0, 0, 3, 4], [0, 0, 0, 0, 0], [0, 0, 0, 0,0], [0, 0, 0, 0, 0]]))
-print(solution([[0,1,1],[0,0,1],[0,0,0]]))
-print(solution([[0,1,2],[0,0,0],[0,0,0]]))
+print(solution([[0, 1, 0, 0, 0, 1], [4, 0, 0, 3, 2, 0], [0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0]]))
+print(solution([[0, 2, 1, 0, 0], [0, 0, 0, 3, 4], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0]]))
+print(solution([[0, 1, 1], [0, 0, 1], [0, 0, 0]]))
+print(solution([[0, 1, 2], [0, 0, 0], [0, 0, 0]]))
+print(solution([[1, 0], [0, 0]]))
